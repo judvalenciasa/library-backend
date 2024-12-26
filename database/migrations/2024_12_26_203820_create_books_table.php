70cdfbea4c2251979a7d1aaff976ac65304b836c
judvalenciasa <judvalenciasa@gmail.com>
2024-12-26 15:50:54 -0500

    Execute of migrations

---
+++ database/migrations/2024_12_26_203820_create_books_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string("title", 200);
+            $table->string("author", 120);
+            $table->date("date_publication");
+            $table->string("gender", 10);
+            $table->string("category", 10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
